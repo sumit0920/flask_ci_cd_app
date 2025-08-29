8125e3338b89f896fc986e714a900b6f7cbf2f95
Sumit Sharma <sumitsharma.aset@gmail.com>
2025-08-29 14:49:03 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,10 @@
-import app
+import werkzeug
+if not hasattr(werkzeug, "__version__"):
+    werkzeug.__version__ = "patched"
+
+from app import app  # import after patch
 
 def test_homepage():
-    client = app.app.test_client()
-    response = client.get('/')
+    client = app.test_client()
+    response = client.get("/")
     assert response.status_code == 200
