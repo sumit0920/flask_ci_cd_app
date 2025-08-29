58c4e12318c7bfa21157b80ebb1ea613afda5689
Sumit Sharma <sumitsharma.aset@gmail.com>
2025-08-29 15:05:08 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,10 @@
 import werkzeug
 if not hasattr(werkzeug, "__version__"):
-    werkzeug.__version__ = "patched"
+    werkzeug.__version__ = "3.x"  # dummy version to satisfy Flask test_client
 
-from app import app  # import after patch
+from app import app
 
 def test_homepage():
-    client = app.test_client()
-    response = client.get("/")
+    tester = app.test_client()
+    response = tester.get("/")
     assert response.status_code == 200
