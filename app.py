c2cc94dfafa9d4656771fbb940344bf7fb9ec2f0
Sumit Sharma <sumitsharma.aset@gmail.com>
2025-08-29 13:31:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
+import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -23,5 +25,5 @@ def delete(task_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
